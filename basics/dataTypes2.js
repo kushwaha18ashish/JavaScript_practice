206a4f9d199635b8ff16bfdb849744a879beab9f
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-12 18:28:52 +0000

    opertions

---
+++ basics/dataTypes2.js
@@ -0,0 +1,17 @@
+//array ,Object ,functions  -Non-primitive DataTypes
+
+const heros=["Ironman","Thor","Captain America","Hulk"]
+
+let myObj={
+    name:"Sohan",
+    age:26,
+}
+
+const myFunction=function(){
+    console.log("Hello World")
+}
+
+myFunction()
+
+console.log(myObj)
+console.log(heros)
---
+++ basics/operations.js
@@ -0,0 +1,14 @@
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+console.log(num2)
+
+console.log("2"==2)
+
+
