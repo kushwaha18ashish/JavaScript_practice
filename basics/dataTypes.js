c7c5b276530c55dfb88d35736907b6f8fcb451c6
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-12 16:44:01 +0000

    variables and datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+let name="Rohan"
+let age=26
+let isEqual=true
+let place;
+
+//number
+//BigInt 
+//string
+//boolean
+//null
+//undefine
+//symbol
+
+//object
+
+console.log(typeof name)
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefined
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=14537
+let accountName="Sohan"
+var accountEmail="sohan@gmail.com"
+accountCity="Delhi"
+let accountState
+
+//prefer not to use var because of issue in block scope and functional scope
+
+console.log(accountId)
+
+console.table([accountId,accountName,accountEmail,accountCity,accountState])
