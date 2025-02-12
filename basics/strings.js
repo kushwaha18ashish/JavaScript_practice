169c570f0531f84bfad5a0e440d289c693c13676
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-12 19:09:49 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,18 @@
+let name="Ashish"
+let repoCnt=20
+
+console.log(`Hello, I am ${name} and my repository count is ${repoCnt}.`)
+
+const username=new String("ashish123dev")
+console.log(username[2])
+console.log(username.length)
+console.log(username.toUpperCase())
+console.log(username.charAt(5))
+console.log(username.indexOf('h'))
+console.log(username.substring(4,8))
+
+let username2="    rohan123royal    "
+console.log(username2)
+console.log(username2.trim())
+console.log(username2.replace('123','-'))
+console.log(username2.includes('ramesh'))
