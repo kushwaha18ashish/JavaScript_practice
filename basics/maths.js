abd259a659a0af066780b2c0e3facc50d593bcc1
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-13 13:39:14 +0000

    maths

---
+++ basics/maths.js
@@ -0,0 +1,11 @@
+//console.log(Math.random())
+//console.log(Math.floor((Math.random()*10)+1))
+
+// let min=10
+// let max=20
+
+// console.log(Math.floor((Math.random()*(max-min)+min)))
+
+//to get random numbers on dice
+
+console.log(Math.floor((Math.random()*5)+1))
