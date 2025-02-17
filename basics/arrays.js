98a64aec0d04ae119a1ebb5d863414c8df295cd9
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-17 17:58:34 +0000

    arrays01

---
+++ basics/arrays.js
@@ -0,0 +1,29 @@
+const arr=[18,26,47,89,11,23,34]
+console.log(arr[2])
+
+const arr2=new Array(57,34,1,23,78,37)
+//console.log(arr2[5])
+
+const heros=["Thor","IronMan","Hulk","CaptainAmerica"]
+
+//Array methods
+
+// heros.push("BlackPanther")
+// console.log(heros)
+// heros.push("SpiderMan")
+// console.log(heros);
+// heros.pop()
+// console.log(heros)
+
+// arr.unshift(96)
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+console.log("A ",arr)
+const arr3=arr.slice(1,3)
+console.log(arr3)
+console.log("B ",arr)
+const arr4=arr.splice(1,3)
+console.log(arr4)
+console.log("C ",arr)
