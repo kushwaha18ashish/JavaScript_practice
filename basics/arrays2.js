670f369df23a3863db62b3e73887d07503993ee1
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-02-22 16:47:04 +0000

    arrays2

---
+++ basics/arrays2.js
@@ -0,0 +1,23 @@
+const marvel=["Ironman","Thor","Hulk"]
+const dc=["Superman","Flash","Batman"]
+
+// marvel.push(dc)
+// console.log(marvel)
+// console.log(marvel[3]);
+// console.log(marvel[3][0]);
+
+// const hero=marvel.concat(dc)
+// console.log(hero);
+
+// const hero2=[...marvel,...dc]
+// console.log(hero2);
+
+// const hero3=[...marvel,...dc,...hero]
+// console.log(hero3);
+
+const hero4=[1,4,6,[8,3,9],8,[1,5,[6,3]]]
+const hero5=hero4.flat(Infinity)
+console.log(hero5);
+
+
+
