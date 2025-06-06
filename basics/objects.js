095e7e7556c443c348c2a33f1d60bddaf8b0250e
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-06-06 12:58:04 +0000

    Objects

---
+++ basics/objects.js
@@ -0,0 +1,43 @@
+const tinderUser={};
+tinderUser.id=24;
+tinderUser.name="Aman";
+tinderUser.status=false;
+
+console.log(tinderUser)
+
+
+
+
+
+// const ob1={1:"A",2:"B"}
+// const ob2={3:"C",4:"D"}
+
+// const ob3=Object.assign(ob1,ob2)
+// console.log(ob3)
+
+// const ob4={...ob1,...ob2};
+// console.log(ob4);
+
+
+
+
+const user=[
+    {
+        id:"1",
+        name:"user1"
+    },
+
+    {
+        id:"2",
+        name:"user2"
+    }
+]
+
+console.log(user[1].name)
+
+
+console.log(Object.keys(tinderUser))
+console.log(Object.keys(user));
+console.log(Object.values(tinderUser));
+
+
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,10 @@
-console.log("Ashish")
+const words="The quick fox jumped over the lazy goat.";
+console.log(words);
+
+console.log(words[2])
+
+const str=words.split(' ')
+console.log(str[2])
+
+const strCpy=words.split()
+console.log(strCpy);
