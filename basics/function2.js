78da31ce05bc700059ea471d95e16ef23dc4b918
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-06-09 08:49:52 +0000

    object In Function

---
+++ basics/function2.js
@@ -0,0 +1,24 @@
+// function sum(...num1){
+//     return num1;
+// }
+
+// function sum(val1,val2,...num1){
+//     return num1;
+// }
+
+// console.log(sum(34,67,12,45));
+
+
+const obj={
+    username:"Rohan",
+    age:"24"
+}
+
+function objectInFunction(anyObject){
+    console.log(`Username is ${anyObject.username} and age is ${anyObject.age}`);
+    
+}
+
+
+objectInFunction(obj)
+
