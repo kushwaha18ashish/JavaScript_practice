e9de67b98d09821e9c28fd829d25377168985bfe
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-06-09 10:30:10 +0000

    iife.js

---
+++ basics/iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log("Database Connected.");
+    
+})();
+
+(()=>{
+    console.log("Database Connected Two.");
+})();
+
+((name)=>{
+    console.log(`Database ${name} Connected Three.`);
+})("Rohan")
