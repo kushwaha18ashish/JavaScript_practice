dcc87a58e2f00b3c30bf2f34701f345694801c75
Ashish Kushwaha <130868880+kushwaha18ashish@users.noreply.github.com>
2025-06-10 12:59:03 +0000

    Loops

--- basics/Loops.js
+++ basics/Loops.js
@@ -1,6 +1,16 @@
 const heros=['Ironman','Thor','Hulk','CaptainAmerica']
 
-for (const hero in heros) {
-    console.log(heros[hero]);
+// for (const hero in heros) {
+//     console.log(heros[hero]);
+    
+// }
+
+for (const key of heros) {
+    console.log(heros[key]);
+    
+}
+
+for (const key of heros) {
+    console.log(key);
     
 }
